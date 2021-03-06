93eab74afc1a83dcb901773624b84c6600292c3c
Henrik Gustavii <henrik.gustavii@everylifetechnologies.com>
2021-03-06 11:51:11 +0000

    Initial Commit

---
+++ TrialExercisesApp/TrialExercisesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialExercisesAppApp.swift
+//  TrialExercisesApp
+//
+//  Created by Henrik Gustavii on 06/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrialExercisesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
